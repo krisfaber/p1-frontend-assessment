1e6ac948ee399c8e4fdb31c0865784752bd67908
Kris Faber <hello@krisfaber.com>
2023-04-14 13:23:43 +0530

    style active nav item

--- frontend/src/view/components/Navbar/NavLink.tsx
+++ frontend/src/view/components/Navbar/NavLink.tsx
@@ -1,6 +1,6 @@
 import { createStyles } from '@mantine/core';
 import classNames from 'classnames';
-import { Link } from 'react-router-dom';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 import { BaseComponent } from '../../../app/interfaces/BaseComponent';
 import { useGenericStyles } from '../../../app/styles/useGenericStyles';
 
@@ -15,15 +15,28 @@ const useStyles = createStyles((theme) => ({
         paddingRight: theme.spacing.md,
         textDecoration: 'none',
         color: 'white',
+        borderBlockEndWidth: 2,
+        borderBlockEndStyle: 'solid',
+        borderBlockEndColor: 'transparent',
+    },
+    linkActive: {
+        borderBlockEndColor: 'white',
     },
 }));
 
 export const NavLink = ({ children, href }: NavLinkProps) => {
     const { classes } = useStyles();
     const { classes: genericClasses } = useGenericStyles();
+    const resolved = useResolvedPath(href);
+    const match = useMatch({ path: resolved.pathname, end: true });
 
     return (
-        <Link to={href} className={classNames(classes.link, genericClasses.hoverable)}>
+        <Link
+            to={href}
+            className={classNames(classes.link, genericClasses.hoverable, {
+                [classes.linkActive]: match,
+            })}
+        >
             {children}
         </Link>
     );
