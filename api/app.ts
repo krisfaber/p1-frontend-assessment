620fe6229905c611e78f8c53295d05a02f586a00
Kris Faber <hello@krisfaber.com>
2023-04-14 15:20:04 +0530

    use cors

--- api/app.ts
+++ api/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import http from "http";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app: Application = express();
 const server = http.createServer(app);
@@ -8,6 +9,7 @@ const server = http.createServer(app);
 // Setup
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 // ANONYMOUS ROUTES
 const authAnonymousRoutes = require("./routes/tickets/tickets");
